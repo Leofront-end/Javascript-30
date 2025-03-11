61a3398c2cc0316dada6a1381b9684d36f03ef78
Leofront-end <ls171258@gmail.com>
2025-03-11 20:26:47 -0300

    19-WebCam

---
+++ 19-UnrealWebcamFun/script.js
@@ -0,0 +1,24 @@
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({video: true , audio: false}).then(localMediaScreen => {
+        console.log(localMediaScreen)
+        video.src = window.URL.createObjectURL(localMediaScreen)
+        video.play()
+    }).catch (err => {
+        console.error(`Nãoooo!!!! ${err}`)
+    })
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth
+    const height = video.videoHeight
+    canvas.width = width
+    canvas.height = height
+}
+
+getVideo()
