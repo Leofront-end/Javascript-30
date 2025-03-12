ce94f1b50c7383c20264a020102defeb9d91b460
Leofront-end <ls171258@gmail.com>
2025-03-12 20:09:11 -0300

    19 - WebCam

--- 19-UnrealWebcamFun/script.js
+++ 19-UnrealWebcamFun/script.js
@@ -22,9 +22,15 @@ function paintToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video,0,0, width, height)
-        const pixels = ctx.getImageData(0,0,width, height)
-        console.log(pixels)
+        let pixels = ctx.getImageData(0,0,width, height)
+        // pixels = redEffect(pixels)
 
+        // pixels = rgbSplit(pixels)
+        // ctx.globalAlpha = 0.1
+
+        pixels = greenScreen(pixels)
+
+        ctx.putImageData(pixels, 0, 0)
     },16)
 }
 
@@ -39,6 +45,45 @@ function takePhoto(){
     link.setAttribute('download', 'handsome')
     link.innerHTML = `<img src="${data}" alt="Handsome Man">`
     strip.insertBefore(link, strip.firstChild)
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i+0] = pixels.data[i + 0] + 100 // R -> Vermelho
+        pixels.data[i+1] = pixels.data[i + 1] - 50 // G -> Verde
+        pixels.data[i+2] = pixels.data[i + 2] * 0.5 // B -> Azul
+    }
+    return pixels
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]  // R -> Vermelho
+        pixels.data[i + 100] = pixels.data[i + 1]  // G -> Verde
+        pixels.data[i - 150] = pixels.data[i + 2] // B -> Azul
+    }
+    return pixels
+}
+
+function greenScreen(pixels) {
+    const levels = {}
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value
+    });
+
+    for (let i = 0 ; i< pixels.data.length ; i += 4){
+        red = pixels.data[i + 0]
+        green = pixels.data[i + 1]
+        blue = pixels.data[i + 2]
+        alpha = pixels.data[i + 3]
+    }
+
+    if (red >= levels.rmin && green >= levels.gmin && blue >= levels.bmin && red <= levels.rmax && green <= levels.gmax && blue <= levels.bmax) {
+        pixels.data[i + 3] = 0
+    }
+
+    return pixels
 }
 
 getVideo()
