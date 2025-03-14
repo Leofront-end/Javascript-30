3b6b95e4e374657d33fdd72caeae926caf49e890
Leofront-end <ls171258@gmail.com>
2025-03-13 21:40:56 -0300

    20 - Audio

--- 20-NativeSpeechRecognition/script.js
+++ 20-NativeSpeechRecognition/script.js
@@ -7,8 +7,10 @@ let p = document.createElement('p')
 const words = document.querySelector('.words')
 words.appendChild(p)
 
-recognition.addEventListener('results', e => {
-    console.log(e)
+recognition.addEventListener('result', e => {
+    console.log(e.results)
+    const transcript = Array.from(e.results).map(result => result[0]).map(result => result.transcript)
+    console.log(transcript)
 })
 
 recognition.start()
