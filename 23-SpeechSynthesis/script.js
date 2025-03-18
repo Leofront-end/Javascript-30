15d5a48d45e394e182d279e6d39cb77ad19b2b6c
Leofront-end <ls171258@gmail.com>
2025-03-18 20:48:49 -0300

    21-Voz

---
+++ 23-SpeechSynthesis/script.js
@@ -0,0 +1,20 @@
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+const voicesDropdown = document.querySelector('[name="voice"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
+const speakButton = document.querySelector('#spea   k');
+const stopButton = document.querySelector('#stop');
+msg.text = document.querySelector('[name="text"]').value
+
+function populateVoices() {
+    voices = this.getVoices()
+    voicesDropdown.innerHTML = voices.map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`).join('')
+    
+}
+
+function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value)
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices)
+voicesDropdown.addEventListener('change', setVoice)
