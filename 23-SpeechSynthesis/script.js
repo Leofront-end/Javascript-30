22da2ab95bdd054757ed2c4c188b063a9587a7ca
Leofront-end <ls171258@gmail.com>
2025-03-19 20:26:24 -0300

    23-Voz

--- 23-SpeechSynthesis/script.js
+++ 23-SpeechSynthesis/script.js
@@ -2,7 +2,7 @@ const msg = new SpeechSynthesisUtterance();
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector('#spea   k');
+const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
 msg.text = document.querySelector('[name="text"]').value
 
@@ -14,7 +14,24 @@ function populateVoices() {
 
 function setVoice() {
     msg.voice = voices.find(voice => voice.name === this.value)
+    toggle()
+}
+
+function toggle (startOVer = true) {
+    speechSynthesis.cancel()
+    if (startOVer) {
+        speechSynthesis.speak(msg)
+    }
+}
+
+function setOption() {
+    console.log(this.name, this.value)
+    msg[this.name] = this.value
+    toggle()
 }
 
 speechSynthesis.addEventListener('voiceschanged', populateVoices)
 voicesDropdown.addEventListener('change', setVoice)
+options.forEach(option => option.addEventListener('change', setOption))
+speakButton.addEventListener('click',toggle)
+stopButton.addEventListener('click',toggle.bind(null, false))
